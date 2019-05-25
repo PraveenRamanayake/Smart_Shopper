d2217d885d1b3370cba8c184b9d09daabe11d09d
PraveenRamanayake <38110293+praveenramanayake@users.noreply.github.com>
2019-05-26 01:38:32 +0530

    commit

--- app/src/main/java/praveen/com/smartshopper/LoginActivity.java
+++ app/src/main/java/praveen/com/smartshopper/LoginActivity.java
@@ -12,8 +12,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+       setContentView(R.layout.activity_login);
+      /*   Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -23,7 +23,7 @@ public class LoginActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });   */
     }
 
 }
